66cda2fdb8edbd878060bd36896683603f81e916
David Pereira <62812324+Davod3@users.noreply.github.com>
2024-03-28 13:06:29 +0000

    Added create user user_controller

--- user_controller/userHandler.py
+++ user_controller/userHandler.py
@@ -2,6 +2,26 @@ import connexion
 from models.user_id_cart_body import UserIdCartBody
 from models.user import User
 
+import os
+import grpc
+
+from user_service_pb2 import (
+    CreateUserRequest,
+    AuthenticateUserRequest,
+    PayCartRequest,
+    AddItemCartRequest,
+    DeleteItemCartRequest,
+    GetCartContentRequest,
+)
+
+from user_service_pb2_grpc import UserStub
+
+user_service_host = os.getenv("USER_SERVICE_HOST", "localhost")
+user_service_port = os.getenv("USER_SERVICE_PORT", "50051")
+user_service_channel = grpc.insecure_channel(f"{user_service_host}:{user_service_port}")
+
+client = UserStub(user_service_channel)
+
 def addToCart(user_id):
 
     if connexion.request.is_json:
@@ -23,7 +43,20 @@ def createUser():
     if connexion.request.is_json:
         user = User.from_dict(connexion.request.get_json())
 
-    return 'TESTING'
+        request = CreateUserRequest(username=user.username, password=user.password, address=user.address)
+        response = client.CreateUser(request)
+
+        if(response.response_code == 0):
+            return 'Success', 200
+        elif(response.response_code == 1):
+            return 'Username already exists', 403
+        elif(response.response_code == 2):
+            return 'Invalid user parameters', 400
+        else:
+            return 'Service unavailable. Try again later', 500
+
+    else:
+        return 'Invalid request body', 400
 
 def authenticateUser(username, password):
     return 'Testing'
