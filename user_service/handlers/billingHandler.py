5c9665935c96848c52ecbc363a0cbcf2086de536
David Pereira <62812324+Davod3@users.noreply.github.com>
2024-03-30 14:14:59 +0000

    Added payment_service proto to user_service and changed location

--- user_service/handlers/billingHandler.py
+++ user_service/handlers/billingHandler.py
@@ -1,3 +1,20 @@
-def initiatePayment():
-    #TODO - Talk with billing_service to initiate payment
+import os
+import grpc
+
+
+from payment_service_pb2 import (
+    PaymentRequest,
+    CardDetails
+)
+
+from payment_service_pb2_grpc import PaymentServiceStub
+
+#payment_service_host = os.getenv("PAYMENT_SERVICE_HOST", "localhost")
+#payment_service_port = os.getenv("PAYMENT_SERVICE_PORT", "50052")
+#user_repository_channel = grpc.insecure_channel(f"{payment_service_host}:{payment_service_port}")
+
+#client = PaymentServiceStub(user_repository_channel)
+
+def initiatePayment(user_id):
+
     return None
