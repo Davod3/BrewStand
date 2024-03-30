97cb12f078a822845892c60d96c6722da499c686
AzunyY <59543274+AzunyY@users.noreply.github.com>
2024-03-30 01:50:34 +0000

    changes to payment_Service

--- payment_service/main.py
+++ payment_service/main.py
@@ -6,9 +6,12 @@ import payment_service_pb2
 import payment_service_pb2_grpc
 
 from payment_service_pb2 import (
-    ProcessPayment,
-    StorePayment,
-    
+    PaymentRequest,
+    PaymentResponse,
+    InvoiceRequest,
+    InvoiceResponse,
+    UserInvoicesRequest,
+    UserInvoicesResponse,
 )
 
 import paymentHandler
@@ -16,21 +19,49 @@ import paymentHandler
 class PaymentService(payment_service_pb2_grpc.PaymentServicer):
 
     def ProcessPayment(self, request, context):
-        payment_result = paymentHandler.process_payment(
+        payment_success, card_last_four = paymentHandler.process_payment(
             user_id=request.user_id,
             amount=request.amount,
             currency=request.currency,
             items_id=request.items_id,
-            card_details=request.card_details
+            card_details=request.card_details 
         )
 
-        if payment_result[0]:
-            return payment_service_pb2.PaymentResponse(
-                success=True,
-                payment_details=payment_result[1]
-            )
-        else:
+        if not payment_success:
             return payment_service_pb2.PaymentResponse(success=False)
+
+        order_channel = grpc.insecure_channel('order_service_endpoint') 
+        order_stub = order_service_pb2_grpc.OrderServiceStub(order_channel)
+        order_response = order_stub.CreateOrder(...) 
+
+         if not order_response.success:
+            return payment_service_pb2.PaymentResponse(success=False)
+
+        repository_channel = grpc.insecure_channel('')
+        repository_stub = payment_repository_pb2_grpc.PaymentRepositoryServiceStub(repository_channel)
+        invoice_data = payment_repository_pb2.InvoiceData(
+            payment_details=payment_repository_pb2.PaymentData(
+                amount=request.amount,
+                currency=request.currency,
+                item_ids=request.items_id,
+                card_last_four=card_last_four  # Here you use the obtained last four digits
+            ),
+            user_id=request.user_id,
+            order_id=order_response.order_id
+        )
+        store_invoice_response = repository_stub.StoreInvoice(invoice_data)
+
+        if store_invoice_response:
+            return payment_service_pb2.PaymentResponse(
+                success=True
+                invoice_data=invoice_data
+            )
+
+        else:
+            context.set_code(grpc.StatusCode.ABORTED)
+            context.set_details("Payment processing failed")
+            return payment_service_pb2.PaymentResponse(success=False)
+
 
     def GetInvoice(self, request, context):
         try:
@@ -68,6 +99,7 @@ def serve():
 
     server.add_insecure_port("[::]:50052")
     server.start()
+    print("Payment Service running on port 50052")
     server.wait_for_termination()
 
 if __name__ == "__main__":
