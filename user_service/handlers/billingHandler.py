951875bde6b74b7ab06a593fac3833ac8ed59715
David Pereira <62812324+Davod3@users.noreply.github.com>
2024-03-30 15:57:33 +0000

    Finished skeleton for PayCart user_service

--- user_service/handlers/billingHandler.py
+++ user_service/handlers/billingHandler.py
@@ -1,6 +1,7 @@
 import os
 import grpc
 
+import handlers.userHandler as UserHandler
 
 from payment_service_pb2 import (
     PaymentRequest,
@@ -17,16 +18,64 @@ from payment_service_pb2_grpc import PaymentServiceStub
 
 def initiatePayment(user_id, card_number, card_expiry, card_cvc):
 
-    #Get user_id and card_details
-    #Get cart contents
-
     response_code = 0
-    invoice_id = 123
+    invoice_id = 0
     price = 0
     order_id = 0
     customer_id = ''
     fiscal_address = ''
     details = ''
+
+    #Get cart contents
+    (code, cart_contents, cost) = UserHandler.getCartContent(user_id)
+
+    if(code == 0):
+
+        user = UserHandler.getUserByID(user_id)
+
+        if(len(cart_contents) > 0):
+
+            items = list()
+
+            for cart_item in cart_contents:
+                items.append(cart_item.batch_id)
+
+            '''
+            UNCOMMENT WHEN PAYMENT SERVICE IS READY
+
+            request = PaymentRequest(user_id = user_id,
+                                     amount = cost,
+                                     currency = 'EUR',
+                                     items_id = items,
+                                     card_details = CardDetails(card_number=card_number,
+                                                                card_expiry=card_expiry,
+                                                                card_cvc=card_cvc))
+            
+            response = client.ProcessPayment(request)
+
+            if(response.success):
+                #Prepare response
+
+                invoice_id = 0
+                price = response.invoice.invoice_details.amount
+                order_id = response.invoice.order_id
+                customer_id = response.invoice.user_id
+                fiscal_address = user.address
+                details = f"{response.invoice.invoice_details.currency}:{response.invoice.invoice_details.card_last_four}"
+
+            else:
+                # Invalid Order
+                response_code = 3
+
+                '''
+
+        else:
+            # Cart is empty
+            response_code = 2
+
+    else:
+        #User not found
+        response_code = 1
 
     return (response_code,
             invoice_id,
