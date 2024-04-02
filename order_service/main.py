6dac4e0c6f3547e5175b81df231aa201e288eead
Miguel Cut <miguelcut44@gmail.com>
2024-04-02 06:07:32 +0100

    Fixed GetOrder from OrderService

--- order_service/main.py
+++ order_service/main.py
@@ -5,29 +5,67 @@ from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
 from order_service_pb2 import (
-    GetOrderResponse,
-    GetOrderResponse,
-    CreateOrderResponse,
+    GetOrderServiceRequest,
+    GetOrderServiceResponse,
+    GetOrdersServiceRequest,
+    GetOrdersServiceResponse,
+    CreateOrderServiceRequest,
+    CreateOrderServiceResponse,
+    OrderDetails,
+    ItemDetails
 )
 
+import order_service_pb2
 import order_service_pb2_grpc
+
+import os
+import order_repository_pb2
+from order_repository_pb2_grpc import OrderRepositoryStub
 
 class OrderService(order_service_pb2_grpc.OrderServicer):
 
+    def __init__(self):
+        self.order_repository_host = os.getenv("ORDER_REPOSITORY_HOST", "localhost")
+        self.order_repository_port = os.getenv("ORDER_REPOSITORY_PORT", "50064")
+        self.order_repo_stub = OrderRepositoryStub(grpc.insecure_channel(f"{self.order_repository_host}:{self.order_repository_port}"))
+
+    def convertOrderToDetails(self, order):
+        items = []
+        for item in order.items:
+            items.append(ItemDetails(itemID=item.itemID, volume=item.volume))
+        
+        return OrderDetails(
+            order_id=order.order_id,
+            user_id=order.user_id,
+            items=items,
+            shipDate=order.shipDate,
+            status=order.status,
+            complete=order.complete,
+            destinationAddress=order.destinationAddress
+        )
+
     def GetOrder(self, request, context):
-        # TO DO
-        order_id = request.order_id
-        return GetOrderResponse(response_code=0)
+        try:
+            order_id = request.order_id
+            
+            response = self.order_repo_stub.GetOrder(order_repository_pb2.GetOrderRequest(order_id=order_id))
+            convertedOrder = self.convertOrderToDetails(response.order)
+            return GetOrderServiceResponse(response_code=response.response_code, order=convertedOrder)
+        except grpc.RpcError as e:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"Failed to get order: {e}")
+            return GetOrderServiceResponse(response_code=1)
+
 
     def GetOrders(self, request, context):
         # TO DO
         user_id = request.user_id
-        return GetOrdersResponse(response_code=0)
+        return GetOrdersServiceResponse(response_code=0)
 
     def CreateOrder(self, request, context):
         # TO DO
         user_id = request.user_id
-        return CreateOrderResponse(response_code=0)
+        return CreateOrderServiceResponse(response_code=0)
 
 def serve():
 
