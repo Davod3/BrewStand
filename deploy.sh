5114a6852cf7aaf7dc69049498c3c6f04cfdd185
Davod3 <davidpereira.pereira4@gmail.com>
2024-04-09 16:43:14 +0100

    Created deployment script and prepared inventory_repository
    Co-authored-by: mScuT
    Co-authored-by: AzunyY
    Co-authored-by: joaquimSimoes
    Co-authored-by: Tania-Araujo

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+# Inventory Repository
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_repository:latest -f inventory_repository/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_repository:latest
