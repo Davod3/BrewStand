67077ca120556a9d899c40d314278455f18bbae8
Davod3 <davidpereira.pereira4@gmail.com>
2024-04-09 22:22:40 +0100

    Added inventory service to deployment
    Co-authored-by: mScuT
    Co-authored-by: AzunyY
    Co-authored-by: joaquimSimoes
    Co-authored-by: Tania-Araujo

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 # Inventory Repository
 docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_repository:latest -f inventory_repository/Dockerfile .
 docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_repository:latest
+
+#Inventory Service
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_service:latest -f inventory_service/Dockerfile .
+docker push  europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_service:latest
