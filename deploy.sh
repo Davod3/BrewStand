25ecda0060a427e2c8e68cb36e7b5e83df31d96b
Davod3 <davidpereira.pereira4@gmail.com>
2024-04-10 23:51:46 +0100

    Added docker push for remaining services
    Co-authored-by: mScuT
    Co-authored-by: AzunyY
    Co-authored-by: joaquimSimoes
    Co-authored-by: Tania-Araujo

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,47 @@ docker push  europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/bre
 #Inventory Controller
 docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_controller:latest -f inventory_controller/Dockerfile .
 docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/inventory_controller:latest
+
+#Order Controller
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_controller:latest -f order_controller/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_controller:latest
+
+#Order Service
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_service:latest -f order_service/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_service:latest
+
+#Order Repository
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_repository:latest -f order_repository/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/order_repository:latest
+
+#Payment Controller
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_controller:latest -f payment_controller/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_controller:latest
+
+#Payment Service
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_service:latest -f payment_service/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_service:latest
+
+#Payment Repository
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_repository:latest -f payment_repository/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/payment_repository:latest
+
+#Review Controller
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/review_controller:latest -f review_controller/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/review_controller:latest
+
+#Review Service
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/review_service:latest -f review_service/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/review_service:latest
+
+#User Controller
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_controller:latest -f user_controller/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_controller:latest
+
+#User Service
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_service:latest -f user_service/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_service:latest
+
+#User Repository
+docker build --tag europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_repository:latest -f user_repository/Dockerfile .
+docker push europe-southwest1-docker.pkg.dev/cloud-computing-project-416422/brewstand-repo/user_repository:latest
