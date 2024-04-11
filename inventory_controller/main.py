d5039087460910e625f2b39fdbda2c86c47bdfc3
Davod3 <davidpereira.pereira4@gmail.com>
2024-04-11 19:17:22 +0100

    Added health check endpoint to all controllers

--- inventory_controller/main.py
+++ inventory_controller/main.py
@@ -5,5 +5,9 @@ app = config.connex_app
 app.add_api(config.basedir / "inventory_api.yaml")
 app.app.json_encoder = encoder.JSONEncoder
 
+@app.route('/')
+def health_check():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("3001"), debug=True)
