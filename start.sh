c72c3025cbeb86f8e5ccf6f11adeca330a84d142
AzunyY <azunynyan@hotmail.com>
2024-05-07 00:44:46 +0000

    secrets

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+# Exporte os segredos para variáveis de ambiente
+export INVENTORY_DB_PASSWORD=$(gcloud secrets versions access latest --secret="Inventory_db")
+export ORDER_DB_PASSWORD=$(gcloud secrets versions access latest --secret="Order_db")
+export PAYMENT_ORDER_SECRET=$(gcloud secrets versions access latest --secret="Payment_order")
+export USER_DB_PASSWORD=$(gcloud secrets versions access latest --secret="User_db")
+
+
 docker compose up --build -d
